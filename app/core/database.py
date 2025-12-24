02ae42b5f6f66346026c12d9c62516572674fffc
Oriol Morros Vilaseca <OM368@student.aru.ac.uk>
2025-12-24 19:31:28 +0100

    Update database.py

--- app/core/database.py
+++ app/core/database.py
@@ -7,10 +7,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL is not set")
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -19,10 +16,3 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
